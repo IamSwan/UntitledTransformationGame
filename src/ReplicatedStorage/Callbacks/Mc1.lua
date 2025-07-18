b3cf6587f2b7e72fa0d96ac45eaee82a26be3d49
Swan Sarli <swan.sarli@epitech.eu>
2025-07-18 11:25:34 +0200

    :feat: mc keybinds

---
+++ src/ReplicatedStorage/Callbacks/Mc1.lua
@@ -0,0 +1,26 @@
+-- Client
+local player = game.Players.LocalPlayer
+local inputBinder = require(game.ReplicatedStorage.Modules.InputBinder)
+local alienPlaylistManager = require(game.ReplicatedStorage.Modules.AlienPlaylistManager)
+local cooldownModule = require(game.ReplicatedStorage.Modules.Cooldown)
+
+return function(action: string, state: Enum.UserInputState, object: InputObject)
+    if state ~= Enum.UserInputState.Begin then return end
+    if not player:GetAttribute("Master") then return end
+    if not cooldownModule:IsFinished(game.Players.LocalPlayer, "Busy") or not cooldownModule:IsFinished(player, "Transform") then
+		print("busy.")
+		return
+	end
+    local alien = player:GetAttribute("Mc1")
+    if not alien then
+        warn("No alien selected for Mc1 action.")
+        return
+    end
+    local alienIndex = alienPlaylistManager:GetIndexFromAlien(player, alien)
+    if not alienIndex then
+        warn("Alien not found in playlist.")
+        return
+    end
+    player.Character:SetAttribute("CurrentSelection", alienIndex)
+    inputBinder:RunBindlessAction("Transform", state)
+end
